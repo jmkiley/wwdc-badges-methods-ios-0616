a4ab5dd4fc06a9162b519aa9abbaed659fd21090
jmkiley <jordankiley@gmail.com>
2016-05-09 17:33:31 -0400

    Done.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,46 @@
  
  */
 
+- ( NSString * )badgeForSpeaker:(NSString *)speaker {
+    NSMutableString *helloMyName = [ @"Hello, my name is" mutableCopy ] ;
+    
+    [helloMyName appendFormat:@" %@.", speaker];
+    
+    NSLog(@"%@", helloMyName);
+    
+    return helloMyName;
+}
+
+// I want to create an array, create a (mutable?) string with the badge message, and have a for loop for each speaker's name, add it to message, add to array
+- ( NSArray * )badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *speakerBadges = [ NSMutableArray new ];
+    NSString *hello = @"Hello, my name is";
+    for ( NSUInteger i = 0; i < [ speakers count ]; i++ ) {
+        NSString *speaker = speakers[i];
+        NSMutableString *helloMyName = [hello mutableCopy];
+        [ helloMyName appendFormat:@" %@.", speaker];
+        [ speakerBadges addObject:helloMyName ];
+        NSLog(@"%@", helloMyName);
+    }
+    return speakerBadges;
+}
+
+- ( NSArray * )greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSString *greetings = @"Welcome";
+    NSMutableString *room = [ @"You'll be in dressing room" mutableCopy ] ;
+    
+    NSMutableArray *roomAssignments = [ NSMutableArray new ] ;
+    
+    for ( NSUInteger i = 0; i < [ speakers count ]; i++ ) {
+        NSUInteger *roomNo = i + 1;
+        NSString *speaker = speakers[i];
+        NSMutableString *welcome = [ greetings mutableCopy ];
+        
+        [ welcome appendFormat:@", %@! %@ %lu.", speaker, room, roomNo ];
+        [ roomAssignments addObject:welcome ];
+        NSLog(@"%@", welcome);
+    }
+    
+    return roomAssignments;
+}
 @end
